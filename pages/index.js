a3e4038e771a64b4b05ed3f27d22cc5c16ec6ee6
RuariDouglas <70585404+RuariDouglas@users.noreply.github.com>
2021-04-04 16:56:05 +0200

    Create 404 page

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,25 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>This is the home page</h1>
+      <h1 className={styles.title}>This is the home page</h1>
+      <p className={styles.text}>
+        Th’art nesh thee nay lad soft lad wacken thi sen up t’foot o’ our
+        stairs. Nay lad where’s tha bin. Th’art nesh thee a pint ‘o mild any
+        rooad t’foot o’ our stairs. Where there’s muck there’s brass t’foot o’
+        our stairs ah’ll gi’ thee a thick ear. Ah’ll learn thi tintintin tell
+        thi summat for nowt soft lad mardy bum. Chuffin’ nora ah’ll box thi ears
+        soft lad ee by gum tell thi summat for nowt ah’ll gi’ thee a thick ear.
+      </p>
+      <p className={styles.text}>
+        Th’art nesh thee nay lad soft lad wacken thi sen up t’foot o’ our
+        stairs. Nay lad where’s tha bin. Th’art nesh thee a pint ‘o mild any
+        rooad t’foot o’ our stairs. Where there’s muck there’s brass t’foot o’
+        our stairs ah’ll gi’ thee a thick ear. Ah’ll learn thi tintintin tell
+        thi summat for nowt soft lad mardy bum. Chuffin’ nora ah’ll box thi ears
+        soft lad ee by gum tell thi summat for nowt ah’ll gi’ thee a thick ear.
+      </p>
       <Link href="/artists">
-        <a>See Artists Listing</a>
+        <a className={styles.btn}>See Artists Listing</a>
       </Link>
     </div>
   );
